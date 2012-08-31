87f9dae95ab656e54c56a6ce4756355c7019604f
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-08-31 18:37:34 +1000

    *Actually* stop reporting overall sample
    The mInterval needs to be initialised before the logic in ReportOML does
    anything with it...
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- src/main.cpp
+++ src/main.cpp
@@ -173,13 +173,13 @@ int main( int argc, char **argv ) {
 #ifdef HAVE_LIBOML2
     if (ext_gSettings->mReportMode == kReport_OML ||
 		    ext_gSettings->mReportMode == kReport_OML_full) {
-	    OML_init (&argc, (const char**) argv);
-	    OML_set_measurement_points(ext_gSettings);
-	    OML_inject_application(argc, argv);
 	    /* If no interval has been required, set it to 1s, 
 	     * it makes more sense to report more samples and let OML deal with them*/
 	    if (0 == ext_gSettings->mInterval)
 		    ext_gSettings->mInterval = 1;
+	    OML_init (&argc, (const char**) argv);
+	    OML_set_measurement_points(ext_gSettings);
+	    OML_inject_application(argc, argv);
     }
 #endif
 
