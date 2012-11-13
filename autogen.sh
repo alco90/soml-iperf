8b7f956c305f30defff6b8036bfeca1393a30fc2
Olivier Mehani <olivier.mehani@nicta.com.au>
2012-11-13 13:28:19 +1100

    Use git-version-gen
    refs: #833
    
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,10 @@
 
 # autogen.sh: Automatically generate all build files from configure.ac
 
+# Import Gnulib modules
+gnulib-tool --update --quiet --quiet
+chmod a+x build-aux/git-version-gen
+
 # aclocal doesn't like it if you -I the system aclocal directory
 SYS_ACLOCAL_DIR=`aclocal --print-ac-dir`
 for i in m4 /usr/local/share/aclocal; do
