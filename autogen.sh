05fc51ca101442526c93e4ac8c3a74174258a96d
Olivier Mehani <olivier.mehani@nicta.com.au>
2013-07-05 16:05:57 +1000

    Typo in autogen.sh
    Signed-off-by: Olivier Mehani <olivier.mehani@nicta.com.au>

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 
 rm -rf autom4te.cache/
 # Let the user know what we do, tersely
-echo "gnulib-tool: updating portability libary files" >&2
+echo "gnulib-tool: updating portability library files" >&2
 gnulib-tool --update --quiet --quiet >/dev/null
 chmod a+x build-aux/git-version-gen
 if [ x$1 != x--quick ]; then
